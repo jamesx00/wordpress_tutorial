39ee19bb7997288e0881f8d0c50fc01d48bd305c
Siwat Tansiri <tansirijames@gmail.com>
2022-04-05 09:56:28 +0700

    feat: Add program post type

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -3,6 +3,7 @@
 function university_post_types()
 
 {
+    // Event post type
     register_post_type('event', array(
         'supports' => array(
             'title', 'editor', 'excerpt',
@@ -20,6 +21,27 @@ function university_post_types()
             'edit_item' => 'Edit Event',
             'all_items' => 'All Events',
             'singular_name' => 'Event',
+        ),
+    ));
+
+    // Program post type
+    register_post_type('program', array(
+        'supports' => array(
+            'title', 'editor',
+        ),
+        'rewrite' => array(
+            'slug' => 'programs',
+        ),
+        'has_archive' => true,
+        'public' => true,
+        'menu_icon' => 'dashicons-awards',
+        'show_in_rest' => true,
+        'labels' => array(
+            'name' => 'Programs',
+            'add_new_item' => 'Add New Program',
+            'edit_item' => 'Edit Program',
+            'all_items' => 'All Programs',
+            'singular_name' => 'Program',
         ),
     ));
 }
