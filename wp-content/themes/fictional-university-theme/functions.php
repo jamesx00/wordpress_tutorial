971c9a1a2c06a1352aa9ced4ca9aa4b8eb2298a3
Siwat Tansiri <tansirijames@gmail.com>
2022-04-05 11:04:53 +0700

    feat: Add program archive and single page

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -37,6 +37,12 @@ function university_adjust_queries($query)
             ),
         ));
     }
+
+    if (!is_admin() and is_post_type_archive('program') and $query->is_main_query()) {
+        $query->set('posts_per_page', -1);
+        $query->set('order_by', 'title');
+        $query->set('order', 'ASC');
+    }
 }
 
 add_action('pre_get_posts', 'university_adjust_queries');
